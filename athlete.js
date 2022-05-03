ec3583a1d9d3952d92eb9d808247681fe45f7eb3
JessicaVitanza <103486794+JessicaVitanza@users.noreply.github.com>
2022-05-03 08:46:08 +0200

    athlete age

--- athlete.js
+++ athlete.js
@@ -14,42 +14,45 @@ this.isRetired = isRetired
 
     }
 
-    calculateAge() {
-        const now = new Date();
-        const year = now.getFullYear();
-        const age = year - this.yob;
-        return age;
-      }
+
+calculateAge() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const age = year - this.yob;
+    return age;
+}
 
 
 
-      bestResult(){
-        return this.palmares.filter(this.keepBest);
-     };
- 
-     keepBest(result){
-         if (result >= 10){
-             return true
-         } else {
-             return false
-         }
-     }
+bestResult(athlete){ 
+
+  let result = 0;
+                
+  for (const max of this.palmares) {
+                    
+   if (result < max) {
+       result = max;
+  }
+  } 
+            
+    return result;
+}
 
 
+toString(){
 
-      toString(){
-
-        const genere = this.gender === 'f' ? 'femmina' : 'maschio';
-        const retired = this.isRetired === true ? 'yes' : 'no';
+  const genere = this.gender === 'f' ? 'femmina' : 'maschio';
+  const retired = this.isRetired === true ? 'yes' : 'no';
     
-          const athleteString = 'Nome: ' + this.name + '\n' +
-                                'Cognome: ' + this.surname + '\n' +
-                                'Età: ' + this.calculateAge() + '\n' +
-                                'Genere: ' + genere + '\n' +
-                                'Sport: ' + this.sport + '\n' +
-                                'Ritirato: ' + retired + '\n' +
-                                'Best result: ' + this.bestResult();
+  const athleteString = 'Nome: ' + this.name + '\n' +
+                        'Cognome: ' + this.surname + '\n' +
+                        'Età: ' + this.calculateAge() + '\n' +
+                        'Genere: ' + genere + '\n' +
+                        'Sport: ' + this.sport + '\n' +
+                        'Ritirato: ' + retired + '\n' +
+                         'Best result: ' + this.bestResult();
 
-        return athleteString;
-      }
+  return athleteString;
+
+  }
 }
--- scriptathlete.js
+++ scriptathlete.js
@@ -1,4 +1,4 @@
-const athlete1 = new Athlete('Usain', 'Bolt', 1980, 'atletica', [3, 5 , 7, 2, 10], 'm', true);
+const athlete1 = new Athlete('Usain', 'Bolt', 1986, 'atletica', [8, 5 , 7, 9, 10], 'm', true);
 
 console.log(athlete1);
 
@@ -10,7 +10,7 @@ console.log('Best result:', athlete1.bestResult());
 
 
 
-const athlete2 = new Athlete('Michael', 'Phelps', 1986, 'nuoto', [1, 3 , 1, 2, 1], 'm', true);
+const athlete2 = new Athlete('Michael', 'Phelps', 1985, 'nuoto', [1, 3 , 1, 2, 1], 'm', true);
 
 console.log(athlete2);
 
@@ -22,7 +22,7 @@ console.log('Best result:', athlete2.bestResult());
 
 
 
-const athlete3 = new Athlete('Yuri', 'Chechi', 1962, 'ginnastica ritmica', [10, 8 , 6, 9, 10], 'm', false);
+const athlete3 = new Athlete('Yuri', 'Chechi', 1969, 'ginnastica ritmica', [10, 8 , 6, 9, 10], 'm', false);
 
 console.log(athlete3);
 
